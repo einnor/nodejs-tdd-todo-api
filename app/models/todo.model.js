d5ade361cdcb074f652e6606fbe928c53b1ba15c
Ronnie Nyaga <ronnienyaga@gmail.com>
2017-05-21 14:45:23 +0300

    Added structure and set up the Todo schema

---
+++ app/models/todo.model.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// defining schema for our todo API
+var TodoSchema = Schema({
+  todo: {
+    type: String
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  created_on: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+// export our model
+var TodoModel = mongoose.model('Todo', TodoSchema);
+
+module.exports = TodoModel;
